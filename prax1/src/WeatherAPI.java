0959df7da593579791e3bfdbc7c52ddd8d56c9e8
frvarb <frvarb@ttu.ee>
2017-09-24 15:22:08 +0300

    initial classes.

---
+++ prax1/src/WeatherAPI.java
@@ -0,0 +1,4 @@
+
+public class WeatherAPI {
+    // Just a placeholder.
+}
---
+++ prax1/src/WeatherAPITest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+
+class WeatherAPITest {
+    @Test
+    public void testWeatherInputNone() {
+
+    }
+
+}
