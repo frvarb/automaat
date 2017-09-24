bf670d9e523cbdfce61435bf02a3365e183c11be
frvarb <frvarb@ttu.ee>
2017-09-24 18:40:21 +0300

    tests made.

--- prax1/src/WeatherAPITest.java
+++ prax1/src/WeatherAPITest.java
@@ -3,8 +3,56 @@ import org.junit.jupiter.api.Test;
 
 class WeatherAPITest {
     @Test
-    public void testWeatherInputNone() {
-
+    public void testWeatherInputNowEmpty() {
+        WeatherAPI.weatherInput = "";
+        assertEquals(WeatherAPI.weatherOutput.now, null);
     }
+
+    @Test
+    public void testWeatherInputNowWrongLetter() {
+        WeatherAPI.weatherInput = "11k:220";
+        assertEquals(WeatherAPI.weatherOutput.now, null);
+    }
+
+    @Test
+    public void testWeatherInputNowWrongSplitter() {
+        WeatherAPI.weatherInput = "111,220";
+        assertEquals(WeatherAPI.weatherOutput.now, null);
+    }
+
+    @Test
+    public void testWeatherInputNowNull() {
+        WeatherAPI.weatherInput = null;
+        assertEquals(WeatherAPI.weatherOutput.now, null);
+    }
+
+    @Test
+    public void testWeatherInputNowRight() {
+        WeatherAPI.weatherInput = "110:220";
+        assertEquals(WeatherAPI.weatherOutput.now, "18 Degrees right now");
+    }
+
+    @Test
+    public void testWeatherInputPredictionWrong() {
+        WeatherAPI.weatherInput = null;
+        assertEquals(WeatherAPI.weatherOutput.prediction, null);
+    }
+
+    @Test
+    public void testWeatherInputPredictionRightTallinn() {
+        WeatherAPI.weatherInput = "120:220";
+        assertEquals(WeatherAPI.weatherOutput.prediction, "11-16 degrees " + date.today + day +
+                ", 12-14 degrees " + date.today + day + day +
+                ", 10-15 degrees " +  date.today + day + day + day + " Tallinn.");
+    }
+
+    @Test
+    public void testWeatherInputPredictionRightHelsinki() {
+        WeatherAPI.weatherInput = "110:220";
+        assertEquals(WeatherAPI.weatherOutput.prediction, "10-18 degrees " + date.today + day +
+                ", 9-15 degrees " + date.today + day + day +
+                ", 8-12 degrees " +  date.today + day + day + day + " Helsinki.");
+    }
+
 
 }
